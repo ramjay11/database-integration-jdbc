622c974c0fb1954ccd3059f1be1621eab40e522c
Ramjay <ramjayromorosa@yahoo.com>
2023-11-01 17:25:46 +0800

    committing

---
+++ src/main/java/com/ramjava/database/integration/jdbc/DatabaseIntegrationJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.database.integration.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseIntegrationJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseIntegrationJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramjava/database/integration/jdbc/DatabaseIntegrationJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.database.integration.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseIntegrationJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
